fc614aad95827e530cf0b83599c9b37b98e78dfe
Lumonza <96143475+Lumonza@users.noreply.github.com>
2023-04-23 20:56:00 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello linux")
-=======
->>>>>>> parent of b846357 (change file main)
-=======
->>>>>>> parent of b846357 (change file main)
-=======
->>>>>>> parent of b846357 (change file main)
+
